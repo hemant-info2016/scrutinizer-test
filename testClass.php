7a00d60bc1fc8de7f0cd5fc42e628059f387edc8
hemant-info2016 <37438726+hemant-info2016@users.noreply.github.com>
2020-03-04 14:46:41 +0530

    Update testClass.php

--- testClass.php
+++ testClass.php
@@ -8,10 +8,21 @@ class testClass {
         
     }
     
-    public function __get($name, $value) {
-        
-        if(isset($this->$name) !== true) {
-            return $this->$name;
-        }
+    private $data = array();
+
+    public function __get($key) {
+        echo "get $key\n";
+        return array_key_exists($key, $this->data) ? $this->data[$key] : null;
     }
+
+    public function __set($key, $value) {
+        echo "set $key = $value\n";
+        $this->data[$key] = $value;
+    }
+
+    public function __isset($key) {
+       echo sprintf("isset $key ( returns %b )", array_key_exists($key, $this->data));
+       return array_key_exists($key, $this->data);
+    }
+    
 }
