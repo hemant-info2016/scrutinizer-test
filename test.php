7b6e79be9525a161a2d6602dc8b5ee9917a68603
hemant-info2016 <37438726+hemant-info2016@users.noreply.github.com>
2020-03-04 15:07:17 +0530

    Update test.php

--- test.php
+++ test.php
@@ -5,4 +5,12 @@ include_once("testClass.php");
 $objTestClass = new testClass();
 
 $objTestClass->test1 = 'tet1';
+
+/**
+ * show off @property, @property-read, @property-write
+ *
+ * @property mixed $regular regular read/write property
+ * @property-read int $foo the foo prop
+ * @property-write string $bar the bar prop
+ */
 $objTestClass->test2 = 'tet2';
